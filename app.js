86d120c8f5b6ee6bd4b268539c2d724494586f54
unknown <rafipramudya164@gmail.com>
2026-09-21 15:21:07 +0700

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+function greet(name) {
+  return "Halo, " + name;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function main() {
+  console.log(greet("Dunia"));
+  console.log(add(2, 3));
+}
+
+main();
